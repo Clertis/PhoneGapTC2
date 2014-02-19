ff18851825c9de8a68d32c039f796925e0c07b7e
clertis <clertis@hotmail.com>
2014-02-19 11:54:19 -0300

    Mais uma modificação

--- www/js/index.js
+++ www/js/index.js
@@ -49,10 +49,13 @@ var app = {
 
     takePicture: function() {
       navigator.camera.getPicture( function( imageURI ) {
-        alert( imageURI );
 		var largeImage = document.getElementById('largeImage');
 		largeImage.style.display = 'block';
 		largeImage.src = "data:image/jpeg;base64," + imageURI;
+		
+		var smallImage = document.getElementById('smallImage');
+		smallImage.style.display = 'block';
+		smallImage.src = "data:image/jpeg;base64," + imageURI;
 		
       },
       function( message ) {
